26409d647fd03124f9f2a75d7d754cf79f281228
Steven-D-Morgan <47731992+Steven-D-Morgan@users.noreply.github.com>
2023-05-18 18:01:58 -0400

    Update Nextcloud_SampleConfig.php

--- Cheat Sheets/Nextcloud_SampleConfig.php
+++ Cheat Sheets/Nextcloud_SampleConfig.php
@@ -1,25 +1,5 @@
 <?php
 
-/**
- * This configuration file is only provided to document the different
- * configuration options and their usage.
- *
- * DO NOT COMPLETELY BASE YOUR CONFIGURATION FILE ON THIS SAMPLE. THIS MAY BREAK
- * YOUR INSTANCE. Instead, manually copy configuration switches that you
- * consider important for your instance to your working ``config.php``, and
- * apply configuration options that are pertinent for your instance.
- *
- * This file is used to generate the configuration documentation.
- * Please consider following requirements of the current parser:
- *  * all comments need to start with `/**` and end with ` *\/` - each on their
- *    own line
- *  * add a `@see CONFIG_INDEX` to copy a previously described config option
- *    also to this line
- *  * everything between the ` *\/` and the next `/**` will be treated as the
- *    config option
- *  * use RST syntax
- */
-
 $CONFIG = [
 
 
